fdf3dae7c0cb83b4ef2244407cc21a38dd9b7f17
Miika <48839377+discapes@users.noreply.github.com>
2020-05-11 12:35:10 +0300

    refactored tons of stuff, everything works now

--- Project X/Direct3DX9.cpp
+++ Project X/Direct3DX9.cpp
@@ -7,31 +7,9 @@
 #include "imgui/imgui_impl_win32.h"
 #include "Window.h"
 #include <directxmath.h>
+#include "Camera.h"
 
 namespace DirectX {
-	namespace Camera {
-		D3DXVECTOR3 pos(0.0f, 0.0f, -2.0f);
-		D3DXVECTOR3 targetAbsPos(0.0f, 0.0f, 0.0f);
-		D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
-
-		D3DXVECTOR4 targetRelPos(0.0f, 0.0f, 0.f, 0);
-		D3DXVECTOR4 targetRelPosRight(0.0f, 0.0f, 0.f, 0);
-		D3DXVECTOR4 targetRelPosUp(0.0f, 0.0f, 0.f, 0);
-
-		D3DXMATRIX targetAngle;
-		D3DXMATRIX targetRightAngle;
-		D3DXMATRIX targetUpAngle;
-
-		D3DXMATRIX rightAngle;
-		D3DXMATRIX upAngle;
-
-
-
-		D3DXMATRIX view;
-
-		float angleX{ 0 };
-		float angleY{ 0 };
-	}
 
 	IDirect3D9* d3d9 = NULL;
 	IDirect3DDevice9* d3dDevice = NULL;
@@ -49,6 +27,8 @@ namespace DirectX {
 		d3dDevice->SetRenderState(D3DRS_LIGHTING, false);
 		//d3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
 		initVertices();
+
+		// init camera
 		D3DXMatrixRotationYawPitchRoll(&Camera::rightAngle, D3DXToRadian(90), 0, 0);
 		D3DXMatrixRotationYawPitchRoll(&Camera::upAngle, 0, D3DXToRadian(-90), 0);
 
@@ -85,79 +65,10 @@ namespace DirectX {
 			Menu::isOpen = !Menu::isOpen;
 		prevState = GetAsyncKeyState(VK_INSERT);
 
-		// GET ANGLES
-		if (GetAsyncKeyState(VK_UP)) // UP
-			Camera::angleX -= timeDelta * 1000.f;
-		if (GetAsyncKeyState(VK_DOWN)) // DOWN
-			Camera::angleX += timeDelta * 1000.f;
-		if (GetAsyncKeyState(VK_RIGHT)) // RIGHT
-			Camera::angleY += timeDelta * 1000.f;
-		if (GetAsyncKeyState(VK_LEFT)) // LEFT
-			Camera::angleY -= timeDelta * 1000.f;
+		Camera::handleControls(timeDelta);
+		DirectX::d3dDevice->SetTransform(D3DTS_VIEW, &Camera::view); // set D3DTS_VIEW to view
 
-		if (GetAsyncKeyState(VK_RETURN)) { // ENTER
-			Camera::pos = { 0.0f, 0.0f, -2.0f };
-			Camera::angleY = 0;
-			Camera::angleX = 0;
-		}
-
-		// NORMALIZE ANGLES
-		if (Camera::angleY >= 6.28f)
-			Camera::angleY = 0.0f;
-
-		if (Camera::angleX >= 6.28f)
-			Camera::angleX = 0.0f;
-
-		if (Camera::angleY <= -6.28f)
-			Camera::angleY = 0.0f;
-
-		if (Camera::angleX <= -6.28f)
-			Camera::angleX = 0.0f;
-
-
-		//TODO move thsee in to a function cameraControls. Like even the stuff up from here
-		// CAMERA MOVEMENT AND ANGLE
-		D3DXMatrixRotationYawPitchRoll(&Camera::targetAngle, Camera::angleY, Camera::angleX, 0);		// get the target angle from camera
-		Camera::targetRightAngle = Camera::targetAngle * Camera::rightAngle;
-		D3DXVec3Transform(&Camera::targetRelPos, &D3DXVECTOR3(0.0f, 0.0f, 1.f), &Camera::targetAngle);	// get the target position from camera
-		D3DXVec3Transform(&Camera::targetRelPosRight, &D3DXVECTOR3(0.0f, 0.0f, 1.f), &Camera::targetRightAngle);
-
-		{ // MOVEMENT
-			if (GetAsyncKeyState(0x57)) { // W
-				Camera::pos.x += Camera::targetRelPos.x * timeDelta * 1000.f;
-				Camera::pos.z += Camera::targetRelPos.z * timeDelta * 1000.f;
-			}
-			if (GetAsyncKeyState(0x53)) { // S
-				Camera::pos.x -= Camera::targetRelPos.x * timeDelta * 1000.f;
-				Camera::pos.z -= Camera::targetRelPos.z * timeDelta * 1000.f;
-			}
-			if (GetAsyncKeyState(0x44)) { // D
-				Camera::pos.x += Camera::targetRelPosRight.x * timeDelta * 1000.f;
-				Camera::pos.z += Camera::targetRelPosRight.z * timeDelta * 1000.f;
-			}
-			if (GetAsyncKeyState(0x41)) {// A
-				Camera::pos.x -= Camera::targetRelPosRight.x * timeDelta * 1000.f;
-				Camera::pos.z -= Camera::targetRelPosRight.z * timeDelta * 1000.f;
-			}
-			if (GetAsyncKeyState(VK_LSHIFT)) {// Left Shift
-				Camera::pos.y += timeDelta * 1000.f;
-			}
-			if (GetAsyncKeyState(VK_LCONTROL)) {// Left Control
-				Camera::pos.y -= timeDelta * 1000.f;
-			}
-		}
-
-		{ // ANGLE
-			Camera::targetAbsPos.x = Camera::pos.x + Camera::targetRelPos.x;
-			Camera::targetAbsPos.y = Camera::pos.y + Camera::targetRelPos.y;	// get the absolute position of target
-			Camera::targetAbsPos.z = Camera::pos.z + Camera::targetRelPos.z;
-		}
-
-		// SET VIEW
-		D3DXMatrixLookAtLH(&Camera::view, &Camera::pos, &Camera::targetAbsPos, &Camera::up); // get view
-		d3dDevice->SetTransform(D3DTS_VIEW, &Camera::view); // set D3DTS_VIEW to view
-
-		Crosshair::move(Camera::pos);
+		Crosshair::move((Vertex)Camera::pos);
 
 		//Clear the window to 0x00000000 (black) 0x00000055 (dark blue
 		d3dDevice->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0x00000000, 1.0f, 0);
--- Project X/Direct3DX9.h
+++ Project X/Direct3DX9.h
@@ -4,38 +4,40 @@
 
 namespace DirectX {
 
-	namespace Camera {
-		extern D3DXVECTOR3 pos;
-		extern D3DXVECTOR3 targetAbsPos;
-		extern D3DXVECTOR3 up;
+	//Vertex structure
+	struct Vertex {
+		FLOAT x, y, z;
+		D3DCOLOR color;    //add color to vertex structure
+		Vertex() {
+			x = 0;
+			y = 0;
+			z = 0;
+			color = D3DCOLOR_RGBA(255, 255, 255, 255);
+		};
+		Vertex(float _x, float _y, float _z, D3DCOLOR _color) {
+			x = _x;
+			y = _y;
+			z = _z;
+			color = _color;
+		}
+		Vertex operator+(Vertex d) {
+			return { x + d.x, y + d.y, z + d.z, d.color };
+		}
+		Vertex operator-(Vertex d) {
+			return { x - d.x, y - d.y, z - d.z, d.color };
+		}
+		Vertex operator*(float d) {
+			return { x * d, y * d, z * d, color };
+		}
+		explicit Vertex(D3DXVECTOR3 _vector) : x{ _vector.x }, y{ _vector.y }, z{ _vector.z }, color{D3DCOLOR_RGBA(255, 255, 255, 255)} {};
+	};
 
-		extern D3DXMATRIX view;
-
-		extern float angleX;
-		extern float angleY;
-
-		extern D3DXVECTOR4 targetRelPos;
-		extern D3DXVECTOR4 targetRelPosRight;
-
-		extern D3DXMATRIX targetAngle;
-		extern D3DXMATRIX targetRightAngle;
-
-		extern D3DXMATRIX rightAngle;
-		extern D3DXMATRIX upAngle;
-	}
-
-    //Vertex structure
-    struct Vertex {
-        FLOAT x, y, z;
-        D3DCOLOR color;    //add color to vertex structure
-    };
-
-    //Define the Flexible vertex format
-    inline const DWORD VertexFVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
+	//Define the Flexible vertex format
+	inline const DWORD VertexFVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
 
 	int setupScene();
 	void cleanUp();
 	int renderScene(float timeDelta);
-    int initializeD3D(HWND hwnd); 
+	int initializeD3D(HWND hwnd);
 	void initVertices();
 }
